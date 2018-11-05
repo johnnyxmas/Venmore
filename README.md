33389cd02281446bfa0277dcfd4ee5f17437f6f9
Johnny Xmas <johnny+github@johnnyxmas.net>
2018-11-04 23:06:13 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Venmore
+Scripts for scraping the Venmo v1 and v5 APIs 
+
+These scripts leverage one-time use account creation to generate API access tokens, which are for some reason placed in HTTP response headers when the New Account Confirmation page is delivered. 
+
+API v1 seems to be the most stable, whereas v5 seems to lock out extremely fast, regardless of token. 
+
+See Johnny Xmas' talk "Shut Up and Take My Money: Scraping the Venmo Public Feed" from Hackfest Canada 10 (2018) for detailed info. 
+
+Slide deck from the above talk located in the [Talk_Decks](https://github.com/johnnyxmas/Talk_Decks/tree/master/2018/Scraping%20the%20Venmo%20Public%20Feed) repository 
---
+++ venmore_api_v5.py
@@ -0,0 +1,93 @@
+#!/bin/python3
+
+import requests,json,time,random,os
+from urllib import parse
+
+#Get your initial epoch values from https://venmo.com/api/v5/public to avoid requesting too wide of a range
+since_epoch = 1541392845 #Most recent epoch time to work backwards from 
+until_epoch = 1541392843 #Epoch stopping point (furthest time in the past)
+
+if not os.path.exists("thenmo_results"):
+    os.makedirs("thenmo_results")
+
+session = requests.Session()
+
+#Disposable account creation to get api token:
+def new_account():
+        global session
+        tel_interchange = random.randint(100,999)
+        tel_line = random.randint(1000,9999)
+        username = random.randint(1000000,99999999999999)
+        password = random.randint(10000000000,99999999999999)
+        username = f'{username}@email.com'
+        client_id = 10 #unsure what this does, but it's necessary. 1, 10 and 2899 are valid values. New account seems to always get 10. 
+        account_creation_url = 'https://venmo.com/api/v5/users'
+        
+        account_payload = {
+                'email' :username,
+                'first_name' :'gregory',
+                'last_name' :'evans',
+                'password' :password, 
+                'phone' :f'(708) {tel_interchange}-{tel_line}',
+                'client_id' :client_id
+        }
+
+        r = session.post(account_creation_url, data = account_payload)
+ 
+        if r.status_code == 200: #all good
+            response = json.loads(r.content)
+            result = response['access_token']
+            print("Got Access Token")
+        else: #Probably a bad/used phone number. Try a new one. 
+            print(r.content)
+            tel_line += 1
+            time.sleep(1)
+            result = 0
+        return(result)
+
+#Create new account, get token
+api_token = new_account()
+while api_token == 0:
+    api_token = new_account() 
+
+feed_url = 'https://venmo.com/api/v5/public'
+next_page = feed_url
+
+
+#these headers are not necessary, but. . .JIC
+#If you want to bypass bot detection, randomize the order and contents of these:
+feed_headers = { 
+        'DNT' :'1',
+        'Cache-Control' :'max-age=0',
+        'User-Agent' :'Mozilla/5.0 (iPhone; CPU OS 11_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Mobile/14A346 Safari Line/6.6.1'
+}
+
+#reeead tha feeeed:
+while until_epoch <= since_epoch:
+    feed_params = (
+	('since', since_epoch),
+	('until', until_epoch)
+    )
+
+    print("Trying to get feed. . .")
+    print(feed_params)
+    feed = session.get(next_page, params=feed_params) #get feed
+    print("Success")
+
+    if feed.status_code != 429:
+        results = json.loads(feed.content)
+        next_page = results['paging']['next']
+        feed_data = results['data']
+        f= open("thenmo_results/{}".format(before_id),"w+") 
+        json.dump(results, f)
+        f.close()
+        before_id = parse.parse_qs(parse.urlparse(next_page).query)['since'][0]
+        print(feed_data)
+        print(before_id)
+        time.sleep(3)
+    else:
+        print("Too Many Requests")
+        print(next_page)
+        time.sleep(600)
+        api_token = new_account() # get fresh token
+print("Job Complete!")
